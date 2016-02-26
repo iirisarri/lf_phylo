1e025f26d76601bfb24c828182c862a64a438e04
iirisarri <irisarri.iker@gmail.com>
2016-02-26 17:50:21 +0100

    new

--- get_BL_stats_by_clade_from_tree.pl
+++ get_BL_stats_by_clade_from_tree.pl
@@ -19,14 +19,33 @@ my $in_tree = $ARGV[0] or die $usage;
 my $outgroup = "Callorhinc";
 
 # define clades
-my @teleosts = qw (Takifugu_r Danio_reri Oreochromi);
-my @sarc_fishes = qw (Lepidosire Neoceratod Protopteru Latimeria_);
-my @tetrapods = qw (Anolis_car Canis_fami Dasypus_no Gallus_gal Homo_sapie Loxodonta_ Macropus_e Meleagris_ Monodelphi Mus_muscul Ornithorhy Rana_chine Taeniopygi Xenopus_tr);
+my %teleosts = ("Takifugu_r" => "1",
+				"Danio_reri" => "1",
+				"Oreochromi" => "1");
 
+my %sarc_fishes = (	"Lepidosire" => "1",
+					"Neoceratod" => "1",
+					"Protopteru" => "1",
+					"Latimeria_" => "1");
+
+my %tetrapods = (	"Canis_fami" => "1",
+					"Dasypus_no" => "1",
+					"Gallus_gal" => "1",
+					"Homo_sapie" => "1",
+					"Loxodonta_" => "1",
+					"Macropus_e" => "1",
+					"Meleagris_" => "1",
+					"Monodelphi" => "1",
+					"Mus_muscul" => "1",
+					"Ornithorhy" => "1",
+					"Rana_chine" => "1",
+					"Taeniopygi" => "1",
+					"Xenopus_tr" => "1");
+				
 # read in trees
 my $treeio = new Bio::TreeIO(-file   => "$in_tree",
-						     -format => "newick"
-						    );
+	      		     -format => "newick"
+    );
 
 my $total_length = 0;
 my @branch_lengths = ();
@@ -44,55 +63,47 @@ my $stat_tel = Statistics::Descriptive::Full->new();
 while( my $tree = $treeio->next_tree ) {
 
 
-	# root the tree
+    # root the tree
     my $root = $tree->find_node( -id => $outgroup );
     $tree->reroot( $root );
 
-	# get total branch length
-	$total_length = $tree->total_branch_length;		
-	
-	for my $node ( $tree->get_nodes ) {
+    # get total branch length
+    $total_length = $tree->total_branch_length;		
+    
+    for my $node ( $tree->get_nodes ) {
 
-		# get tetrapod branches
-		foreach my $t ( @tetrapods ) {
-	
-			my $tet = $tree->find_node(-id => $t);
-			# $tet is a Bio::Tree::Node object
-			# depth takes calculates sum of BL till the root
-			my $tet_depth = $tet->depth;
-			print "$t depth: $tet_depth\n";
-			# scape if a particular species is not present in the tree
-			next if (!defined $tet_depth);
-			push (@depths_tetrapods, $tet_depth);
-		}
-		# get sarcopterygian fish branches
-		foreach my $u ( @sarc_fishes ) {
-	
-			my $sar = $tree->find_node(-id => $u);
-			# $tet is a Bio::Tree::Node object
-			my $sar_depth = $sar->depth;
-			print "$u depth: $sar_depth\n";
-			next if (!defined $sar_depth);
-			push (@depths_sarc_fishes, $sar_depth);
-		}
-		# get teleost branches
-		foreach my $v ( @teleosts ) {
-	
-			my $tel = $tree->find_node(-id => $v);
-			# $tet is a Bio::Tree::Node object
-			my $tel_depth = $tel->depth;
-			print "$v depth: $tel_depth\n";
-			next if (!defined $tel_depth);
-			push (@depths_teleosts, $tel_depth);
-		}
-		# get all nodes together (also previous ones!)
+       	# get node leafs
+       	if ( $node->is_Leaf ) {
+			
+		my $species = $node->id;
+		my $species_node = $tree->find_node(-id => $species);
+		my $depth = $species_node->depth;
+						
+		# scape undefined depths (often related to rooting)
+		next if (!defined $depth);
+		
+		# get depths for tetrapods, teleosts and sarcopterygian fishes
+		if ( exists $tetrapods{$species} ) {
 
-		my $length = $node->branch_length;
-		# scape undefined branch lengths (root)
-		next if (!defined $length);
-		push (@branch_lengths, $length);
+			push (@depths_tetrapods, $depth);
+		}
+		if ( exists $sarc_fishes{$species} ) {
+
+			push (@depths_sarc_fishes, $depth);
+		}
+		if ( exists $teleosts{$species} ) {
+
+			push (@depths_teleosts, $depth);
+		}
+		
+	# get branch length (for all leaves and internal branches)
+	my $length = $node->branch_length;
+	# scape if branch length is undefined (often related to rooting)
+	next if (!defined $length);
+	push (@branch_lengths, $length);
+	
 	}
-
+	
 	# calculate stats from @branch_lengths
 	$stat_tot->add_data(@branch_lengths);
 	my $mean = $stat_tot->mean();
@@ -124,8 +135,6 @@ while( my $tree = $treeio->next_tree ) {
 	my $diff2 = abs ( $tel_mean - $sar_mean );
 	my $diff3 = abs ( $tet_mean - $tel_mean );
 	
-	
-	
 	# print out stuff
 	print 	"File\tmean_BL\tsd_BL\ttotal_BL\t",
 			"Tetrapods_mean\tTetrapods_sd\tTetrapods_min\tTetrapods_max\t",
@@ -137,7 +146,6 @@ while( my $tree = $treeio->next_tree ) {
 			"$sar_mean\t$sar_sd\t$sar_min\t$sar_max\t",
 			"$tel_mean\t$tel_sd\t$tel_min\t$tel_max\t",
 			"$diff1\t$diff2\t$diff3\n";
-	
 }
 	
 print STDERR "\nDone!\n\n";	
